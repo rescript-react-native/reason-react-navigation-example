4abb94e108e488423420817a79e8061e998115c0
Christoph Knittel <christoph.knittel@cca.io>
2019-10-05 08:25:41 +0200

    npx react-native init NavigationExample --template reason-react-native-template

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+const APP_NAME = "NavigationExample";
+
+AppRegistry.registerComponent(APP_NAME, () => App);
